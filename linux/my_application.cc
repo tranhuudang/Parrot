d60b95356ee5d3d645957f2dad38446569c8a60c
Tran Huu Dang <96557038+tranhuudang@users.noreply.github.com>
2025-02-26 16:55:56 +0700

    fix: inconsistent name on windows

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "flutter_version_manager");
+    gtk_header_bar_set_title(header_bar, "parrot");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "flutter_version_manager");
+    gtk_window_set_title(window, "parrot");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 // import 'package:flutter/material.dart';
 // import 'package:flutter_test/flutter_test.dart';
 //
-// import 'package:flutter_version_manager/main.dart';
+// import 'package:parrot/main.dart';
 //
 // void main() {
 //   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
